2bb145007309161ca1f1eaa97e7c69f9f4106926
tonybui98 <96325836+tonybui98@users.noreply.github.com>
2022-02-21 08:50:42 +0700

    Create project

---
+++ header/header-1.php
@@ -0,0 +1,15 @@
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+  <script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+  <script
+  src="../bootstrap/js/bootstrap.min.js"></script>
+<link href="../fontawesome/css/brands.min.css" />
+<link href="../fontawesome/css/solid.min.css" />
+<link href="../fontawesome/css/fontawesome.min.css" />
+<link href="../bootstrap/css/bootstrap.min.css" />
+<?php 
